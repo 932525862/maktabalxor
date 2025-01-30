a6ebe438dafa8e32d5818553228a532f4836fd43
asrorovuz <asrorovsardor07@gmail.com>
2025-01-30 22:46:12 +0500

    addd .

--- src/pages/news/newsCarousel.tsx
+++ src/pages/news/newsCarousel.tsx
@@ -1,43 +1,57 @@
+import { useNavigate } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import img1 from "../../assets/abouta2.jpg"
 
+import img1 from "../../assets/abouta2.jpg";
 
 //Problem with installation
-import "swiper/css";
-import "swiper/css/pagination";
-import { useNavigate } from "react-router-dom";
+import 'swiper/swiper-bundle.css';
+//@ts-ignore
+import 'swiper/css/pagination';
 
 
+import { Autoplay } from "swiper/modules";
 
 const NewsCarousel: React.FC = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    return (
-        <Swiper
-            slidesPerView={3}
-            spaceBetween={60}
-            // pagination={{ clickable: true }}
-            autoplay={{ delay: 3500 }}
-            loop={true}
-            modules={[Autoplay]}
-            className="mySwiper h-[270px] w-full my-10"
+  return (
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={60}
+      // pagination={{ clickable: true }}
+      autoplay={{ delay: 3500 }}
+      loop={true}
+      modules={[Autoplay]}
+      className="mySwiper h-[270px] w-full my-10"
+    >
+      {Array.from({ length: 9 }, (_, index) => (
+        <SwiperSlide
+          key={index}
+          className="h-full !w-[370px] flex justify-center items-center"
         >
-            {Array.from({ length: 9 }, (_, index) => (
-                <SwiperSlide
-                    key={index}
-                    className="h-full !w-[370px] flex justify-center items-center"
-                >
-                    <div className="flex flex-col items-center justify-center h-full w-full font-bold relative group cursor-pointer" onClick={() => { navigate("/contact"); scrollTo({ top: 0, behavior: "smooth" }) }}>
-                        <img src={img1} alt="Carousel Image" className="w-full h-full object-cover rounded-2xl hover:rounded-4xl transition-all duration-500" />
-                        <span className="font-bold text-xl text-white absolute bottom-5 opacity-100 group-hover:opacity-0 group-hover:invisible visible transition-all duration-300">{"Ho'l Meva Yangiliklari"}</span>
-                        <span className="absolute top-[50%] text-white text-2xl opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-500 flex items-center justify-center">More -&gt;</span>
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    );
+          <div
+            className="flex flex-col items-center justify-center h-full w-full font-bold relative group cursor-pointer"
+            onClick={() => {
+              navigate("/contact");
+              scrollTo({ top: 0, behavior: "smooth" });
+            }}
+          >
+            <img
+              src={img1}
+              alt="Carousel Image"
+              className="w-full h-full object-cover rounded-2xl hover:rounded-4xl transition-all duration-500"
+            />
+            <span className="font-bold text-xl text-white absolute bottom-5 opacity-100 group-hover:opacity-0 group-hover:invisible visible transition-all duration-300">
+              {"Ho'l Meva Yangiliklari"}
+            </span>
+            <span className="absolute top-[50%] text-white text-2xl opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-500 flex items-center justify-center">
+              More -&gt;
+            </span>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default NewsCarousel;
