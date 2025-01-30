d8d5602d5e61d22b62826c5fa896a7ee5a86e341
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2025-01-30 22:11:15 +0500

    Toshmat changed 6

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,5 +1,10 @@
+import NewsCarousel from "./newsCarousel";
+
 const NewsPage = () => {
-  return <div>CatalogPage</div>;
+  return <div className="container max-w-7xl w-full h-screen mx-auto">
+    <div className="text-4xl mt-20 font-bold">Yangiliklar</div>
+    <NewsCarousel />
+  </div>;
 };
 
 export default NewsPage;
