5694783e13f4bc4671d8c14de874632eff0957f1
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2025-02-03 19:18:31 +0500

    Toshmat News tugatildi

--- src/pages/news/newsOne.tsx
+++ src/pages/news/newsOne.tsx
@@ -1,60 +1,79 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import img1 from "../../assets/abouta2.jpg";
 import img2 from "../../assets/about2.jpg";
 import NewsCarousel from "./newsCarousel";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+interface NewsOne {
+    titleUz: string,
+    titleRu: string;
+    titleEn: string;
+    titleAr: string;
+    descriptionUz: string;
+    descriptionRu: string;
+    descriptionEn: string;
+    descriptionAr: string;
+    imageUrl: string;
+    id: number;
+}
+
+
 
 export default function NewsOnePage() {
     const { id } = useParams<{ id: string }>();
+
+    const [newsOne, setNewsOne] = useState<NewsOne>();
+
+    const getNewsOne = async () => {
+        try {
+            const response = await fetch("https://milmax-backend.milmax.uz/api/news/" + id, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+
+            if (response?.ok) {
+                const data = await response.json();
+                setNewsOne(data?.data);
+                console.log(data?.data, "Data from news One Page");
+            }
+            else {
+                const error = await response.json();
+                console.error("Error while fetching data : ", error);
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        getNewsOne();
+    }, [id])
+
+
     return (
-        <div className="max-w-7xl mx-auto container h-full">
-            <h1 className="text-4xl font-bold my-8 md:mt-10 md:mb-0 text-center">Financial Report {id}</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 place-items-center mt-0 max-h-full h-full md:h-[80vh]">
+        <div className="max-w-7xl mx-auto container h-full !mt-[100px]">
+            <h1 className="text-2xl md:text-4xl font-bold my-10 md:mt-10 md:mb-0 text-center">{newsOne && newsOne[`titleUz` as keyof NewsOne]}</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 lg:gap-8 place-items-center mt-0 max-h-full h-full lg:h-[80vh]">
                 {/* <!-- Text Content --> */}
-                <div className="p-5 text-center order-2 md:order-1">
-                    <div className="space-y-6">
-                        <div>
-                            <h2 className="text-2xl font-semibold">Revenue Growth</h2>
-                            <p className="text-gray-600 mt-2">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                                semper ipsum urna, nec cursus dolor dictum nec. Donec luctus
-                                mauris quis cursus.
-                            </p>
-                        </div>
-                        <div>
-                            <h2 className="text-2xl font-semibold">Return on Investment</h2>
-                            <p className="text-gray-600 mt-2">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                                semper ipsum urna, nec cursus dolor dictum nec. Donec luctus
-                                mauris quis cursus.
-                            </p>
-                        </div>
-                        <div>
-                            <h2 className="text-2xl font-semibold">Customer Satisfaction</h2>
-                            <p className="text-gray-600 mt-2">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                                semper ipsum urna, nec cursus dolor dictum nec. Donec luctus
-                                mauris quis cursus.
-                            </p>
-                        </div>
+                <div className="lg:p-5 p-0 text-center order-2 lg:order-1">
+                    <div className="lg:p-5 p-0 text-center order-2 lg:order-1">
+                        {newsOne && (
+                            <div dangerouslySetInnerHTML={{ __html: newsOne["descriptionUz" as keyof NewsOne] || "" }} />
+                        )}
                     </div>
                 </div>
 
                 {/* <!-- Image Content --> */}
-                <div className="space-y-6 flex flex-col order-1 md:order-2">
-                    <div className="w-[350px] md:w-[450px] h-[200px]">
-                        <img
-                            src={img1}
-                            alt="Fruit Image"
-                            className="w-full h-full object-cover rounded-lg shadow-lg"
-                        />
-                    </div>
-                    <div className="w-[350px] md:w-[450px] h-[200px]">
-                        <img
-                            src={img2}
-                            alt="Fruit Image"
-                            className="w-full h-full object-cover rounded-lg shadow-lg"
-                        />
-                    </div>
+                <div className="flex flex-col order-1 lg:order-2 justify-center items-center w-[90%] lg:w-[500px] h-[70%]">
+                    <img
+                        src={newsOne?.imageUrl}
+                        alt={newsOne?.titleUz}
+                        className="w-full h-full object-cover rounded-lg shadow-lg"
+                    />
                 </div>
             </div>
 
